63a1270d3e447a00715f3b7b95d9f7aa2044e6f6
frank06 <me@frank06.net>
2016-05-10 02:41:59 +0600

    three examples

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,31 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import computedFilterByQuery from 'ember-cli-filter-by-query';
 
 export default Ember.Controller.extend({
   
-  filter: Ember.computed('search', function() {
+  filter1: Ember.computed('input1', function() {
     
-    const search = this.get('search');
+    const search = this.get('input1');
+    
+    if (search) {
+      
+      const result = this.get('bank.products').then(function(products) {
+        return products.filter((product) => {
+          return new RegExp(search, "gi").test(product.get('description'));
+        });
+      })
+      
+      return DS.PromiseArray.create({ promise: result });
+    }
+    
+    return this.get('bank.products');
+    
+  }),
+  
+  filter2: Ember.computed('input2', function() {
+    
+    const search = this.get('input2');
     
     if (search) {
       return this.get('bank.products').filter((product) => {
@@ -17,6 +37,6 @@ export default Ember.Controller.extend({
     
   }),
   
-  filteredList: computedFilterByQuery('bank.products', 'description', 'query')
+  filter3: computedFilterByQuery('bank.products', 'description', 'input3')
   
 });
